fa200b1646d21fb3ca10e6af26392274522094ae
Stefanie Carrasco <stefanieanncarrasco@gmail.com>
2018-02-28 12:44:33 -0500

    adding through step 5

--- scripts/api.js
+++ scripts/api.js
@@ -3,14 +3,19 @@
 
 let api = (function () {
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/kevin';
-  return { 
-    BASE_URL: BASE_URL,
+  return {
     getItems: callback => $.getJSON(BASE_URL + '/items', callback),
+    createItem: (name, callback) => {
+      let newItem = JSON.stringify({
+        name: name,
+      });
+      $.ajax({
+        url: BASE_URL + '/items',
+        method: 'POST',
+        contentType: 'application/json',
+        data: newItem,
+        success: callback,
+      });
+    }
   };
 })();
-
-api.getItems(function(data) {
-  console.log(data);
-});
-
-console.log(api.BASE_URL);
--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,18 @@
-/* global shoppingList, store */
+'use strict';
+/* global shoppingList, store, api, $ */
+
 
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
+  api.getItems((items) => {
+    items.forEach((item) => store.addItem(item));
+    shoppingList.render();
+  });
 });
 
-store.items.push(Item.create('apples'));
+// api.createItem('pears', (newItem) => {
+//   api.getItems((items) => {
+//     console.log(items);
+//   });
+// });
